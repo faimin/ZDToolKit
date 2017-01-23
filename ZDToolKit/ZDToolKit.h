559f4cb39d37f565ea42bc2c3bb0d8b66141cb63
符现超 <fuxianchao@gmail.com>
2017-01-23 15:59:05 +0800

    add headerFile

---
+++ ZDToolKit/ZDToolKit.h
@@ -0,0 +1,89 @@
+//
+//  ZDToolKit.h
+//  ZDToolKitDemo
+//
+//  Created by 符现超 on 2017/1/23.
+//  Copyright © 2017年 Zero.D.Saber. All rights reserved.
+//
+
+#ifndef ZDToolKit_h
+#define ZDToolKit_h
+
+// AutoLayout
+#import "UIView+AutoFlowLayout.h"
+#improt "UIView+AutoLayout.h"
+#improt "UIView+FDCollapsibleConstraints.h"
+
+// BlockKitsFuntion
+#improt "NSArray+BlocksKit.h"
+#improt "NSDictionary+BlocksKit.h"
+#improt "NSIndexSet+BlocksKit.h"
+#improt "NSInvocation+BlocksKit.h"
+#improt "NSMapTable+BlocksKit.h"
+#improt "NSMutableArray+BlocksKit.h"
+#improt "NSMutableDictionary+BlocksKit.h"
+#improt "NSMutableIndexSet+BlocksKit.h"
+#improt "NSMutableOrderedSet+BlocksKit.h"
+#improt "NSMutableSet+BlocksKit.h"
+#improt "NSNumber+BlocksKit.h"
+#improt "NSObject+BKAssociatedObjects.h"
+#improt "NSObject+BKBlockExecution.h"
+#improt "NSObject+BKBlockObservation.h"
+#improt "NSOrderedSet+BlocksKit.h"
+#improt "NSSet+BlocksKit.h"
+#improt "NSTimer+BlocksKit.h"
+#improt "UIControl+BlocksKit.h"
+#improt "UIGestureRecognizer+BlocksKit.h"
+
+// **Category**
+// Fundation
+#improt "NSArray+ZDExtend.h"
+#improt "NSInvocation+Block.h"
+#improt "NSObject+DLIntrospection.h"
+#improt "NSObject+JKAutoCoding.h"
+#improt "NSObject+RZBlockKVO.h"
+#improt "NSObject+ZDRuntime.h"
+#improt "NSObject+ZDUtility.h"
+#improt "NSString+ZDUtility.h"
+// UIKit
+#improt "UIButton+ZDUtility.h"
+#improt "UIColor+ZDUtility.h"
+#improt "UIControl+ZDUtility.h"
+#improt "UIImage+ZDUtility.h"
+#improt "UIImageView+FaceAwareFill.h"
+#improt "UIImageView+ZDUtility.h"
+#improt "UILabel+ZDUtility.h"
+#improt "UITextView+ZDUtility.h"
+#improt "UIView+draggable.h"
+#improt "UIView+RZBorders.h"
+#improt "UIView+ZDUtility.h"
+#improt "UIViewController+ZDUtility.h"
+#improt "UIWebView+ZDExtend.h"
+#improt "WKWebView+ZDExtend.h"
+
+// Macros
+#improt "EXTScope.h"
+#improt "metamacros.h"
+#improt "ZDDefine.h"
+
+// Runtime
+#improt "EMCI.h"
+#improt "JRSwizzle.h"
+#improt "NOBRuntime.h"
+
+// Tools
+#improt "UIViewController+ZDPop.h"
+#improt "UIViewController+ZDBackButtonHandler.h"
+#improt "MAKVONotificationCenter.h"
+#improt "ZDActionLabel.h"
+#improt "ZDEdgeLabel.h"
+#improt "ZDFileManager.h"
+#improt "ZDFunction.h"
+#improt "ZDProxy.h"
+#improt "ZDSafe.h"
+#improt "ZDTextView.h"
+#improt "ZDTools.h"
+#improt "ZDWatchdog.h"
+
+
+#endif /* ZDToolKit_h */
