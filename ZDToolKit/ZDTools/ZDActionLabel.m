1c8dcda14b617923cb9321ebe09ce8ad6bab9d25
符现超 <fuxianchao@gmail.com>
2017-03-30 20:27:37 +0800

    update

--- ZDToolKit/ZDTools/ZDActionLabel.m
+++ ZDToolKit/ZDTools/ZDActionLabel.m
@@ -191,92 +191,92 @@ NS_ASSUME_NONNULL_BEGIN
 NS_ASSUME_NONNULL_END
 
 
-/*
+#if 0
  /// CoreText实现图文混排之点击事件：http://www.jianshu.com/p/51c47329203e
- - (void)setTarget:(id)target action:(SEL)selector forRange:(NSRange)range
- {
- if (nil == target || NULL == selector) {
- return;
- }
- 
- NSValue *value = [NSValue valueWithRange:range];
- if (nil == _targetActions) {
- _targetActions = [NSMutableDictionary dictionaryWithCapacity:1];
- }
- NSDictionary *targetActionDict = [_targetActions objectForKey:value];
- if (targetActionDict) {
- [targetActionDict setValue:target forKey:@"target"];
- [targetActionDict setValue:NSStringFromSelector(selector) forKey:@"action"];
- }
- else{
- targetActionDict = @{@"target":target,@"action":NSStringFromSelector(selector)};
- [_targetActions setObject:targetActionDict forKey:value];
- }
- }
- 
- - (NSInteger)indexOfTouchLocation:(CGPoint)location
- {
- if (NULL == _ctFrameRef) {
- NSAttributedString *content = self.attributedText;
- CTFramesetterRef framesetter =  CTFramesetterCreateWithAttributedString((__bridge_retained CFAttributedStringRef)content);
- CGMutablePathRef path = CGPathCreateMutable();
- CGPathAddRect(path, NULL, _textRect);
- //创建CTFrame
- _ctFrameRef = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, content.length), path, NULL);
- CFRelease(framesetter);
- CGPathRelease(path);
- }
- 
- CFArrayRef lines = CTFrameGetLines(_ctFrameRef);
- CGPoint origins[CFArrayGetCount(lines)];
- CTFrameGetLineOrigins(_ctFrameRef, CFRangeMake(0, 0), origins);
- CTLineRef line = NULL;
- CGPoint lineOrigin = CGPointZero;
- CGPathRef path = CTFrameGetPath(_ctFrameRef);
- CGRect rect = CGPathGetBoundingBox(path);
- for (int i= 0; i < CFArrayGetCount(lines); i++){
- CGPoint origin = origins[i];
- CGFloat y = rect.origin.y + rect.size.height - origin.y;
- if ((location.y >= y - _iLineSpacing/2.0f) && (location.y <= y + self.font.lineHeight + _iLineSpacing/2.0f) && (location.x >= origin.x)){
- line = CFArrayGetValueAtIndex(lines, i);
- lineOrigin = origin;
- break;
- }
- }
- 
- location.x -= lineOrigin.x;
- CFIndex index = CTLineGetStringIndexForPosition(line, location);
- return index;
- }
- 
- - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
- {
- [super touchesEnded:touches withEvent:event];
- 
- UITouch *touch = [touches anyObject];
- CGPoint location = [touch locationInView:self];
- 
- NSInteger index = [self indexOfTouchLocation:location];
- if (index != NSNotFound) {
- NSDictionary *targetAction = nil;
- NSArray *targetActionsKeys = [_targetActions allKeys];
- for (NSValue *value in targetActionsKeys) {
- NSRange range = [value rangeValue];
- if (range.location < index && index < range.location + range.length) {
- targetAction = [_targetActions objectForKey:value];
- break;
- }
- }
- 
- if (targetAction) {
- id tartget = [targetAction objectForKey:@"target"];
- SEL selector = NSSelectorFromString([targetAction objectForKey:@"action"]);
- #pragma clang diagnostic push
- #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
- [tartget performSelector:selector withObject:self];
- #pragma clang diagnostic pop
- }
- }
- }
- */
+- (void)setTarget:(id)target action:(SEL)selector forRange:(NSRange)range
+{
+    if (nil == target || NULL == selector) {
+        return;
+    }
+    
+    NSValue *value = [NSValue valueWithRange:range];
+    if (nil == _targetActions) {
+        _targetActions = [NSMutableDictionary dictionaryWithCapacity:1];
+    }
+    NSDictionary *targetActionDict = [_targetActions objectForKey:value];
+    if (targetActionDict) {
+        [targetActionDict setValue:target forKey:@"target"];
+        [targetActionDict setValue:NSStringFromSelector(selector) forKey:@"action"];
+    }
+    else{
+        targetActionDict = @{@"target":target,@"action":NSStringFromSelector(selector)};
+        [_targetActions setObject:targetActionDict forKey:value];
+    }
+}
+
+- (NSInteger)indexOfTouchLocation:(CGPoint)location
+{
+    if (NULL == _ctFrameRef) {
+        NSAttributedString *content = self.attributedText;
+        CTFramesetterRef framesetter =  CTFramesetterCreateWithAttributedString((__bridge_retained CFAttributedStringRef)content);
+        CGMutablePathRef path = CGPathCreateMutable();
+        CGPathAddRect(path, NULL, _textRect);
+        //创建CTFrame
+        _ctFrameRef = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, content.length), path, NULL);
+        CFRelease(framesetter);
+        CGPathRelease(path);
+    }
+    
+    CFArrayRef lines = CTFrameGetLines(_ctFrameRef);
+    CGPoint origins[CFArrayGetCount(lines)];
+    CTFrameGetLineOrigins(_ctFrameRef, CFRangeMake(0, 0), origins);
+    CTLineRef line = NULL;
+    CGPoint lineOrigin = CGPointZero;
+    CGPathRef path = CTFrameGetPath(_ctFrameRef);
+    CGRect rect = CGPathGetBoundingBox(path);
+    for (int i= 0; i < CFArrayGetCount(lines); i++){
+        CGPoint origin = origins[i];
+        CGFloat y = rect.origin.y + rect.size.height - origin.y;
+        if ((location.y >= y - _iLineSpacing/2.0f) && (location.y <= y + self.font.lineHeight + _iLineSpacing/2.0f) && (location.x >= origin.x)){
+            line = CFArrayGetValueAtIndex(lines, i);
+            lineOrigin = origin;
+            break;
+        }
+    }
+    
+    location.x -= lineOrigin.x;
+    CFIndex index = CTLineGetStringIndexForPosition(line, location);
+    return index;
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesEnded:touches withEvent:event];
+    
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self];
+    
+    NSInteger index = [self indexOfTouchLocation:location];
+    if (index != NSNotFound) {
+        NSDictionary *targetAction = nil;
+        NSArray *targetActionsKeys = [_targetActions allKeys];
+        for (NSValue *value in targetActionsKeys) {
+            NSRange range = [value rangeValue];
+            if (range.location < index && index < range.location + range.length) {
+                targetAction = [_targetActions objectForKey:value];
+                break;
+            }
+        }
+        
+        if (targetAction) {
+            id target = [targetAction objectForKey:@"target"];
+            SEL selector = NSSelectorFromString([targetAction objectForKey:@"action"]);
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            [target performSelector:selector withObject:self];
+#pragma clang diagnostic pop
+        }
+    }
+}
+#endif
 
