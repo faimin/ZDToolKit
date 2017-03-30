81d434a4eac0778e2820c578939a20295f80c54e
符现超 <fuxianchao@gmail.com>
2017-03-30 15:26:41 +0800

    update define

--- ZDToolKit/ZDMacros/ZDDefine.h
+++ ZDToolKit/ZDMacros/ZDDefine.h
@@ -8,21 +8,39 @@
 
 #ifndef ZDUtility_ZDDefine_h
 #define ZDUtility_ZDDefine_h
+#endif
+
 #import <pthread.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 
 //-------------------屏幕物理尺寸-------------------------
-//NavBar高度
-#define NavigationBar_HEIGHT 44
 //获取屏幕宽度、高度
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_8_0 // 当前Xcode支持iOS8及以上
-#define kSCREEN_WIDTH   ([[UIScreen mainScreen] respondsToSelector:@selector(nativeBounds)] ? [UIScreen mainScreen].nativeBounds.size.width/[UIScreen mainScreen].nativeScale : [UIScreen mainScreen].bounds.size.width)
-#define kSCREENH_HEIGHT ([[UIScreen mainScreen] respondsToSelector:@selector(nativeBounds)] ? [UIScreen mainScreen].nativeBounds.size.height/[UIScreen mainScreen].nativeScale : [UIScreen mainScreen].bounds.size.height)
-#define kSCREEN_SIZE    ([[UIScreen mainScreen] respondsToSelector:@selector(nativeBounds)] ? CGSizeMake([UIScreen mainScreen].nativeBounds.size.width/[UIScreen mainScreen].nativeScale, [UIScreen mainScreen].nativeBounds.size.height/[UIScreen mainScreen].nativeScale) : [UIScreen mainScreen].bounds.size)
+	#ifndef ZD_SCREEN_WIDTH
+		#define ZD_SCREEN_WIDTH   ([[UIScreen mainScreen] respondsToSelector:@selector(nativeBounds)] ? [UIScreen mainScreen].nativeBounds.size.width/[UIScreen mainScreen].nativeScale : [UIScreen mainScreen].bounds.size.width)
+	#endif
+	
+	#ifndef ZD_SCREENH_HEIGHT
+		#define ZD_SCREENH_HEIGHT ([[UIScreen mainScreen] respondsToSelector:@selector(nativeBounds)] ? [UIScreen mainScreen].nativeBounds.size.height/[UIScreen mainScreen].nativeScale : [UIScreen mainScreen].bounds.size.height)
+	#endif
+
+	#ifndef ZD_SCREEN_SIZE
+		#define ZD_SCREEN_SIZE    ([[UIScreen mainScreen] respondsToSelector:@selector(nativeBounds)] ? CGSizeMake([UIScreen mainScreen].nativeBounds.size.width/[UIScreen mainScreen].nativeScale, [UIScreen mainScreen].nativeBounds.size.height/[UIScreen mainScreen].nativeScale) : [UIScreen mainScreen].bounds.size)
+	#endif
 #else
-#define kSCREEN_WIDTH   ([UIScreen mainScreen].bounds.size.width)
-#define kSCREENH_HEIGHT ([UIScreen mainScreen].bounds.size.height)
-#define kSCREEN_SIZE    ([UIScreen mainScreen].bounds.size)
+	#ifndef ZD_SCREEN_WIDTH
+		#define ZD_SCREEN_WIDTH   ([UIScreen mainScreen].bounds.size.width)
+	#endif
+
+	#ifndef ZD_SCREENH_HEIGHT
+		#define ZD_SCREENH_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+	#endif
+
+	#ifndef ZD_SCREEN_SIZE
+		#define ZD_SCREEN_SIZE    ([UIScreen mainScreen].bounds.size)
+	#endif
 #endif
 
 
@@ -38,13 +56,14 @@
 ///A better version of NSLog
 //refer : http://onevcat.com/2014/01/black-magic-in-macro/
 #ifdef DEBUG
-#define NSLog(format, ...) do {                                                 \
+#define NSLog(format, ...) 														\
+do {                                                 							\
     fprintf(stderr, "<%s : %d> %s\n",                                           \
     [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String],  \
     __LINE__, __PRETTY_FUNCTION__);                                             \
     (NSLog)((format), ##__VA_ARGS__);                                           \
     fprintf(stderr, "----------万恶的分割线---------\n\n");                       \
-    } while (0)
+} while (0)
 #endif
 
 
@@ -62,7 +81,7 @@
   #define ZDAlertLog(...)     ((void)0)
 #endif
 
-#define ZDAssert(condition, ...)														\
+#define ZD_Assert(condition, ...)														\
 	do {																				\
 		if (!(condition))																\
 		{																				\
@@ -79,10 +98,10 @@
 //----------------------系统----------------------------
 
 //获取系统版本
-#define SYSTEMVERSION			[[[UIDevice currentDevice] systemVersion] floatValue]
+#define ZD_SYSTEMVERSION	[[[UIDevice currentDevice] systemVersion] floatValue]
 
 //获取当前语言
-#define CurrentLanguage			([[NSLocale preferredLanguages] objectAtIndex:0])
+#define ZD_CurrentLanguage	([[NSLocale preferredLanguages] objectAtIndex:0])
 
 //判断是真机还是模拟器
 #if TARGET_OS_IPHONE
@@ -109,23 +128,15 @@
 
 //----------------------内存----------------------------
 #pragma mark - 内存
-//使用ARC和不使用ARC
-#if __has_feature(objc_arc)
-//compiling with ARC
-#else
-// compiling without ARC
+
+#if !__has_feature(objc_arc)
+	//释放一个对象
+	#define ZD_SAFE_RELEASE(P)	  \
+		if (P)					  \
+		{						  \
+			[P release], P = nil; \
+		}
 #endif
-
-#define RELEASE_SAFELY(__POINTER) {[__POINTER release]; __POINTER = nil; }
-
-//释放一个对象
-#define SAFE_DELETE(P)		  \
-	if (P)					  \
-	{						  \
-		[P release], P = nil; \
-	}
-
-#define SAFE_RELEASE(x) [x release]; x = nil
 
 //----------------------内存----------------------------
 
@@ -269,16 +280,15 @@
 //----------------------图片----------------------------
 
 //读取本地图片
-#define LOADIMAGE(file, type)	[UIImage imageWithContentsOfFile:[[NSBundle mainBundle]pathForResource:file ofType:type]]
+#define ZD_BUNDLEIMAGE(file, type)	[UIImage imageWithContentsOfFile:[[NSBundle mainBundle]pathForResource:file ofType:type]]
 //定义UIImage对象
-#define ImageNamed(_pointer)	[UIImage imageNamed:_pointer]
+#define ZD_ImageNamed(_imageName)	[UIImage imageNamed:_imageName]
 
-//建议使用前两种宏定义,性能高于后者
 //----------------------图片----------------------------
 
 //----------------------颜色类---------------------------
 /// RGB颜色转换（16进制->10进制）
-#define UIColorFromHEX(rgbValue)                                        \
+#define ZD_UIColorFromHEX(rgbValue)                                     \
 [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0    \
                 green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0       \
                  blue:((float)(rgbValue & 0xFF)) / 255.0 alpha: 1.0]    \
@@ -292,8 +302,8 @@
 //#endif
 
 // 获取RGB颜色
-#define RGBA(r, g, b, a)	[UIColor colorWithRed: r / 255.0f green: g / 255.0f blue: b / 255.0f alpha: a]
-#define RGB(r, g, b)		RGBA(r, g, b, 1.0f)
+#define ZD_RGBA(r, g, b, a)	[UIColor colorWithRed: r / 255.0f green: g / 255.0f blue: b / 255.0f alpha: a]
+#define ZD_RGB(r, g, b)		ZD_RGBA(r, g, b, 1.0f)
 
 //十六进制颜色
 //#define COLOR_RGBA(r, g, b, a) \
@@ -306,31 +316,21 @@
 
 //----------------------其他----------------------------
 
-//方正黑体简体字体定义
-#define FONT(F)						[UIFont fontWithName: @"FZHTJW--GB1-0" size: F]
-
 //设置View的tag属性
-#define VIEWWITHTAG(_OBJECT, _TAG)	[_OBJECT viewWithTag: _TAG]
+#define ZD_VIEWWITHTAG(_OBJECT, _TAG)	[_OBJECT viewWithTag: _TAG]
 //程序的本地化,引用国际化的文件
-#define MyLocal(x, ...)				NSLocalizedString(x, nil)
+#define ZD_LOCAL(x, ...)				NSLocalizedString(x, nil)
 
 //G－C－D
-#define GLOBALQUEUE(block)          dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
-#define MAINQUEUE(block)			dispatch_async(dispatch_get_main_queue(), block)
-
-//NSUserDefaults 实例化
-#define USERDEFAULT                 [NSUserDefaults standardUserDefaults]
+#define ZD_GLOBALQUEUE(block)  	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
+#define ZD_MAINQUEUE(block)		dispatch_async(dispatch_get_main_queue(), block)
 
 //由角度获取弧度 有弧度获取角度
-#define DegreesToRadian(x)          (M_PI * (x) / 180.0)
-#define RadianToDegrees(radian)     ((radian * 180.0) / (M_PI))
+#define ZD_DegreesToRadian(x)          (M_PI * (x) / 180.0)
+#define ZD_RadianToDegrees(radian)     ((radian * 180.0) / (M_PI))
 
-#define LITE_RESOURCE_BUNDLE [[NSBundle mainBundle] pathForResource: \
-	@"LiteResource"													 \
-	ofType:@"bundle"];
-
-#define IS_KIND_OF_CLASS(obj_, Class_) [obj_ isKindOfClass:NSClassFromString(@#Class_)]
-#define URL(urlString_) [NSURL URLWithString:urlString_]
+#define ZD_IS_KIND_OF_CLASS(obj_, Class_) [obj_ isKindOfClass:NSClassFromString(@#Class_)]
+#define ZD_URL(urlString_) [NSURL URLWithString:urlString_]
 
 //TODO宏 (http://blog.sunnyxx.com/2015/03/01/todo-macro/ )
 #define STRINGIFY(S) #S                             // 转成字符串
@@ -340,12 +340,8 @@
 #define KEYWORDIFY try {} @catch (...) {}           // 使宏前面可以加@
 #define TODO(MSG) KEYWORDIFY PRAGMA_MESSAGE(FORMATTED_MESSAGE(MSG))// 最终使用的宏
 
-//defer(swift延迟调用关键字)宏 (http://blog.sunnyxx.com/2014/09/15/objc-attribute-cleanup/ )
-static inline void CleanupBlock(__strong void(^*executeCleanupBlock)()) {
-    (*executeCleanupBlock)();
-}
 
-static inline void dispatch_async_on_main_queue(void (^block)()) {
+static inline void ZD_Dispatch_async_on_main_queue(void (^block)()) {
     if (pthread_main_np()) {
         block();
     }
@@ -354,7 +350,7 @@ static inline void dispatch_async_on_main_queue(void (^block)()) {
     }
 }
 
-static inline void dispatch_sync_on_main_queue(void (^block)()) {
+static inline void ZD_Dispatch_sync_on_main_queue(void (^block)()) {
     if (pthread_main_np()) {
         block();
     }
@@ -363,34 +359,63 @@ static inline void dispatch_sync_on_main_queue(void (^block)()) {
     }
 }
 
+static inline void ZD_PrintViewCoordinateInfo(UIView *view) {
+    NSLog(@"\n frame = %@, bounds = %@, center = %@",
+          NSStringFromCGRect(view.frame),
+          NSStringFromCGRect(view.bounds),
+          NSStringFromCGPoint(view.center)
+    );
+}
+
+//defer(swift延迟调用关键字)宏 (http://blog.sunnyxx.com/2014/09/15/objc-attribute-cleanup/ )
+static inline void CleanupBlock(__strong void(^*executeCleanupBlock)()) {
+    (*executeCleanupBlock)();
+}
+
 /// 出了作用域时执行block
 #ifndef zd_defer
-#define zd_defer \
+	#define zd_defer \
         zd_keywordify \
         __strong void(^executeCleanupBlock)() __attribute__((cleanup(CleanupBlock), unused)) = ^
 #endif
 
 #if DEBUG
-#define zd_keywordify @autoreleasepool {}
+	#define zd_keywordify @autoreleasepool {}
 #else
-#define zd_keywordify @try {} @catch (...) {}
-#endif
+	#define zd_keywordify @try {} @catch (...) {}
 #endif
 
-#define weakObjc(objc_) \
+
+#define zd_weakObjc(objc_) \
 @autoreleasepool {} __weak __typeof__(objc_) self_weak_ = (objc_);
 
-#define strongObjc(objc_) \
+#define zd_strongObjc(objc_) \
 @autoreleasepool {} __strong __typeof__(objc_) self = self_weak_;
 
+
 /// 消除performSelector警告
-#define SuppressPerformSelectorLeakWarning(Stuff)                       \
+#define ZD_SuppressPerformSelectorLeakWarning(Stuff)                    \
 do {                                                                    \
     _Pragma("clang diagnostic push")                                    \
     _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
     Stuff                                                               \
     _Pragma("clang diagnostic pop")                                     \
 } while (0)
+
+/// 消除deprecated方法的警告
+#define ZD_SuppressDeprecatedWarning(Stuff)                             \
+do {                                                                    \
+    _Pragma("clang diagnostic push")                                    \
+    _Pragma("clang diagnostic ignored \"-Wdeprecated-declarations\"")   \
+    Stuff                                                               \
+    _Pragma("clang diagnostic pop")                                     \
+} while (0)
+
+
+
+
+
+
 
 
 
