223c81e829992780607bcb6508482646b2e985ea
faimin <fuxianchao@gmail.com>
2017-05-22 15:20:21 +0800

    Serialize data change to NSKeyedArchiver

--- ZDToolKit/ZDCategory/Foundation/NSDictionary+ZDUtility.m
+++ ZDToolKit/ZDCategory/Foundation/NSDictionary+ZDUtility.m
@@ -11,46 +11,45 @@
 
 @implementation NSDictionary (ZDUtility)
 
-// ref: http://stackoverflow.com/questions/9948698/store-nsdictionary-in-keychain
+// reference: http://stackoverflow.com/questions/9948698/store-nsdictionary-in-keychain
 - (void)storeToKeychainWithKey:(NSString *)aKey {
     // serialize dict
-    NSString *error;
-    NSData *serializedDictionary = [NSPropertyListSerialization dataFromPropertyList:self format:NSPropertyListXMLFormat_v1_0 errorDescription:&error];
+    NSData *serializedDictionary = [NSKeyedArchiver archivedDataWithRootObject:self];
+    //[NSPropertyListSerialization dataWithPropertyList:self format:NSPropertyListXMLFormat_v1_0 options:0 error:&error];
     
     // encrypt in keychain
-    if(!error) {
-        // first, delete potential existing entries with this key (it won't auto update)
-        [self deleteFromKeychainWithKey:aKey];
-        
-        // setup keychain storage properties
-        NSDictionary *storageQuery = @{
-                                       (id)kSecAttrAccount:    aKey,
-                                       (id)kSecValueData:      serializedDictionary,
-                                       (id)kSecClass:          (id)kSecClassGenericPassword,
-                                       (id)kSecAttrAccessible: (id)kSecAttrAccessibleWhenUnlocked
-                                       };
-        OSStatus osStatus = SecItemAdd((CFDictionaryRef)storageQuery, nil);
-        if(osStatus != noErr) {
-            // do someting with error
-        }
+
+    // first, delete potential existing entries with this key (it won't auto update)
+    [self deleteFromKeychainWithKey:aKey];
+    
+    // setup keychain storage properties
+    NSDictionary *storageQuery = @{
+                                   (__bridge id)kSecAttrAccount:    aKey,
+                                   (__bridge id)kSecValueData:      serializedDictionary,
+                                   (__bridge id)kSecClass:          (__bridge id)kSecClassGenericPassword,
+                                   (__bridge id)kSecAttrAccessible: (__bridge id)kSecAttrAccessibleWhenUnlocked
+                                   };
+    OSStatus osStatus = SecItemAdd((__bridge CFDictionaryRef)storageQuery, nil);
+    if(osStatus != noErr) {
+        // do someting with error
     }
 }
 
 + (NSDictionary *)dictionaryFromKeychainWithKey:(NSString *)aKey {
     // setup keychain query properties
     NSDictionary *readQuery = @{
-                                (id)kSecAttrAccount: aKey,
-                                (id)kSecReturnData: (id)kCFBooleanTrue,
-                                (id)kSecClass:      (id)kSecClassGenericPassword
+                                (__bridge id)kSecAttrAccount: aKey,
+                                (__bridge id)kSecReturnData: (id)kCFBooleanTrue,
+                                (__bridge id)kSecClass:      (__bridge id)kSecClassGenericPassword
                                 };
     
-    CFTypeRef serializedDictionary = NULL;
-    OSStatus osStatus = SecItemCopyMatching((__bridge CFDictionaryRef)readQuery, &serializedDictionary);
+    CFDataRef serializedDictionary = NULL;
+    OSStatus osStatus = SecItemCopyMatching((__bridge CFDictionaryRef)readQuery, (CFTypeRef *)&serializedDictionary);
     if(osStatus == noErr) {
         // deserialize dictionary
-        NSString *error;
-        NSDictionary *storedDictionary = [NSPropertyListSerialization propertyListFromData:(__bridge NSData *)(serializedDictionary) mutabilityOption:NSPropertyListImmutable format:nil errorDescription:&error];
-        if (error) NSLog(@"%@", error);
+        NSData *data = (__bridge NSData *)serializedDictionary;
+        NSDictionary *storedDictionary = [NSKeyedUnarchiver unarchiveObjectWithData:data];
+        //[NSPropertyListSerialization propertyListFromData:(__bridge NSData *)(serializedDictionary) mutabilityOption:NSPropertyListImmutable format:nil errorDescription:&error];
         return storedDictionary;
     }
     else {
@@ -62,10 +61,10 @@
 - (void)deleteFromKeychainWithKey:(NSString *)aKey {
     // setup keychain query properties
     NSDictionary *deletableItemsQuery = @{
-                                          (id)kSecAttrAccount:        aKey,
-                                          (id)kSecClass:              (id)kSecClassGenericPassword,
-                                          (id)kSecMatchLimit:         (id)kSecMatchLimitAll,
-                                          (id)kSecReturnAttributes:   (id)kCFBooleanTrue
+                                          (__bridge id)kSecAttrAccount:        aKey,
+                                          (__bridge id)kSecClass:              (__bridge id)kSecClassGenericPassword,
+                                          (__bridge id)kSecMatchLimit:         (__bridge id)kSecMatchLimitAll,
+                                          (__bridge id)kSecReturnAttributes:   (id)kCFBooleanTrue
                                           };
     
     CFArrayRef itemList = NULL;
@@ -74,7 +73,7 @@
     NSArray *itemListArray = (__bridge NSArray *)itemList;
     for (NSDictionary *item in itemListArray) {
         NSMutableDictionary *deleteQuery = [item mutableCopy];
-        [deleteQuery setValue:(id)kSecClassGenericPassword forKey:(id)kSecClass];
+        [deleteQuery setValue:(__bridge id)kSecClassGenericPassword forKey:(__bridge id)kSecClass];
         // do delete
         osStatus = SecItemDelete((CFDictionaryRef)deleteQuery);
         if(osStatus != noErr) {
