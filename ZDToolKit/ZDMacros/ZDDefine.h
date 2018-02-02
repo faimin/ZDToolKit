7c14dad28a74718b10c2b44fa90d30ad501ad413
faimin <fuxianchao@gmail.com>
2018-02-02 13:24:10 +0800

    Macro

--- ZDToolKit/ZDMacros/ZDDefine.h
+++ ZDToolKit/ZDMacros/ZDDefine.h
@@ -15,7 +15,7 @@
 #import <UIKit/UIKit.h>
 
 
-//-------------------屏幕物理尺寸-------------------------
+//MARK:- 屏幕物理尺寸
 //获取屏幕宽度、高度
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_8_0 // 当前Xcode支持iOS8及以上
 	#ifndef ZD_SCREEN_WIDTH
@@ -43,6 +43,7 @@
 	#endif
 #endif
 
+//MARK:- iPhone X
 #define ZD_StatusBarHeight          (ZD_SCREENH_HEIGHT == 812.0 ? 44 : 20)     ///< 状态栏高度
 #define ZD_FringeHeight             (ZD_SCREENH_HEIGHT == 812.0 ? 30 : 0)      ///< 刘海高度
 #define ZD_SafeArea_NaviBarHeight   (ZD_SCREENH_HEIGHT == 812.0 ? 88 : 64)     ///< 导航栏高度
@@ -50,7 +51,7 @@
 #define ZD_SafeArea_TopMargin       (ZD_SCREENH_HEIGHT == 812.0 ? 88 : 64)     ///< 导航栏高度
 #define ZD_SafeArea_BottomMargin    (ZD_SCREENH_HEIGHT == 812.0 ? 34 : 0)      ///< 安全区域下方高度
 
-//-------------------打印日志-------------------------
+//MARK:- Log
 //DEBUG  模式下打印日志,当前行
 #ifdef DEBUG
     #define ZDLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
@@ -73,18 +74,18 @@ do {                                                 							\
 #endif
 
 
-//打印宏展开后的函数
+//MARK:- 打印宏展开后的函数
 #define __toString(x) __toString_0(x)
 #define __toString_0(x) #x
 #define ZDLOG_MACRO(x) NSLog(@"%s=\n%s", #x, __toString(x))
 
-
+//MARK:- Alert
 //DEBUG  模式下打印日志,当前行 并弹出一个警告
 #ifdef DEBUG
-  #define ZDAlertLog(fmt, ...) {UIAlertView *alert = [[UIAlertView alloc]	\
+  #define ZD_AlertLog(fmt, ...) {UIAlertView *alert = [[UIAlertView alloc]	\
 							  initWithTitle:[NSString stringWithFormat:@"%s\n [Line %d] ", __PRETTY_FUNCTION__, __LINE__] message:[NSString stringWithFormat:fmt, ##__VA_ARGS__]  delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil]; [alert show]; }
 #else
-  #define ZDAlertLog(...)     ((void)0)
+  #define ZD_AlertLog(...)     ((void)0)
 #endif
 
 #define ZD_Assert(condition, ...)														\
@@ -99,12 +100,6 @@ do {                                                 							\
 		}																				\
 	} while (0)
 
-//---------------------打印日志--------------------------
-
-//----------------------系统----------------------------
-
-//获取系统版本
-#define ZD_SYSTEMVERSION	[[[UIDevice currentDevice] systemVersion] floatValue]
 
 //获取当前语言
 #define ZD_CurrentLanguage	([[NSLocale preferredLanguages] objectAtIndex:0])
@@ -118,7 +113,10 @@ do {                                                 							\
 //iPhone Simulator
 #endif
 
-//检查系统版本
+//MARK:- 版本判断
+//获取系统版本
+#define ZD_SYSTEMVERSION    [[[UIDevice currentDevice] systemVersion] floatValue]
+
 #define SYSTEM_VERSION_EQUAL_TO(v)					([[[UIDevice currentDevice] systemVersion] \
 	compare:v options:NSNumericSearch] == NSOrderedSame)
 #define SYSTEM_VERSION_GREATER_THAN(v)				([[[UIDevice currentDevice] systemVersion] \
@@ -130,11 +128,8 @@ do {                                                 							\
 #define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)		([[[UIDevice currentDevice] systemVersion] \
 	compare:v options:NSNumericSearch] != NSOrderedDescending)
 
-//----------------------系统----------------------------
 
-//----------------------内存----------------------------
-#pragma mark - 内存
-
+//MARK:- 释放
 #if !__has_feature(objc_arc)
 	//释放一个对象
 	#define ZD_SAFE_RELEASE(P)	  \
@@ -144,8 +139,7 @@ do {                                                 							\
 		}
 #endif
 
-//----------------------内存----------------------------
-
+//MARK:- 单例
 //----------------------单例----------------------------
 
 #ifndef SHARED_SERVICE
@@ -283,16 +277,14 @@ do {                                                 							\
 	}
 #endif
 
-//----------------------图片----------------------------
 
-//读取本地图片
+//MARK:- 读取本地图片
 #define ZD_BUNDLEIMAGE(file, type)	[UIImage imageWithContentsOfFile:[[NSBundle mainBundle]pathForResource:file ofType:type]]
 //定义UIImage对象
 #define ZD_IMAGENAMED(_imageName)	[UIImage imageNamed:@#_imageName]
 
-//----------------------图片----------------------------
 
-//----------------------颜色类---------------------------
+//MARK:- RGB
 /// RGB颜色转换（16进制->10进制）
 ///  Create UIColor with a hex string.
 ///
@@ -303,34 +295,32 @@ do {                                                 							\
                  blue:((float)(0x##hexValue & 0xFF)) / 255.0                \
                 alpha: 1.0]
 
-
 // 获取RGB颜色
 #define ZD_RGBA(r, g, b, a)	[UIColor colorWithRed: r / 255.0f green: g / 255.0f blue: b / 255.0f alpha: a]
 #define ZD_RGB(r, g, b)		ZD_RGBA(r, g, b, 1.0f)
 
-//----------------------颜色类--------------------------
 
-//----------------------其他----------------------------
-
-//设置View的tag属性
-#define ZD_VIEWWITHTAG(_OBJECT, _TAG)	[_OBJECT viewWithTag: _TAG]
 //程序的本地化,引用国际化的文件
 #define ZD_LOCAL(x, ...)				NSLocalizedString(x, nil)
 
-//GCD
+//MARK:- GCD
 #define ZD_GLOBAL_QUEUE(block)  	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
 #define ZD_MAIN_QUEUE(block)		dispatch_async(dispatch_get_main_queue(), block)
 #define ZD_CREATE_SERIAL_QUEUE(_label, QOS_CLASS_DEFAULT)    dispatch_queue_create(#_label, dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_SERIAL, QOS_CLASS_USER_INITIATED, 0))
 #define ZD_CREATE_CONCURRENT_QUEUE(_label, QOS_CLASS_DEFAULT)   dispatch_queue_create(#_label, dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_CONCURRENT, QOS_CLASS_USER_INITIATED, 0))
 
-//由角度获取弧度 有弧度获取角度
+//MARK:- 由角度获取弧度/弧度获取角度
 #define ZD_DegreesToRadian(x)          (M_PI * (x) / 180.0)
 #define ZD_RadianToDegrees(radian)     ((radian * 180.0) / (M_PI))
 
+//MARK:- Kindof
 #define ZD_IS_KIND_OF_CLASS(obj_, Class_) [obj_ isKindOfClass:NSClassFromString(@#Class_)]
+
+//MARK:- URL
 #define ZD_URL(urlString_) [NSURL URLWithString:urlString_]
 
-//TODO宏 (http://blog.sunnyxx.com/2015/03/01/todo-macro/ )
+//MARK:- TODO
+//(http://blog.sunnyxx.com/2015/03/01/todo-macro/)
 #define STRINGIFY(S) #S                             // 转成字符串
 #define DEFER_STRINGIFY(S) STRINGIFY(S)             // 需要解两次才解开的宏
 #define PRAGMA_MESSAGE(MSG) _Pragma(STRINGIFY(message(MSG)))
@@ -338,6 +328,7 @@ do {                                                 							\
 #define KEYWORDIFY try {} @catch (...) {}           // 使宏前面可以加@
 #define TODO(MSG) KEYWORDIFY PRAGMA_MESSAGE(FORMATTED_MESSAGE(MSG))// 最终使用的宏
 
+//MARK:- 超出作用域后执行
 //defer(swift延迟调用关键字)宏 (http://blog.sunnyxx.com/2014/09/15/objc-attribute-cleanup/ )
 /// 注意`ZD_CleanupBlock`函数的入参是`cleanup`所修饰变量的地址,类型要一样
 NS_INLINE void ZD_CleanupBlock(__strong void(^*executeCleanupBlock)()) {
@@ -360,7 +351,7 @@ NS_INLINE void ZD_CleanupBlock(__strong void(^*executeCleanupBlock)()) {
         __strong void(^executeCleanupBlock)() __attribute__((cleanup(ZD_CleanupBlock), unused)) = ^
 #endif
 
-
+//MARK:- 弱引用
 #ifndef zd_keywordify
     #if DEBUG
         #define zd_keywordify @autoreleasepool {}
@@ -368,7 +359,6 @@ NS_INLINE void ZD_CleanupBlock(__strong void(^*executeCleanupBlock)()) {
         #define zd_keywordify @try {} @catch (...) {}
     #endif
 #endif
-
 
 #ifndef zd_weakTarget
 #define zd_weakTarget(objc_)  \
@@ -380,10 +370,32 @@ zd_keywordify __weak __typeof__(objc_) weak##_##objc_ = (objc_);
 zd_keywordify __strong __typeof__(weak##_##objc_) objc_ = weak##_##objc_;
 #endif
 
-
+//MARK:- main函数之前执行
 #define ZD_ExcuteBeforeMainFunction __attribute__((constructor))
 
+//MARK:- 类型推断
+// https://pspdfkit.com/blog/2017/even-swiftier-objective-c/ && https://medium.com/@maicki/type-inference-with-auto-type-55a38ef56372
+#if defined(__cplusplus)
+#define zdlet auto const
+#else
+#define zdlet const __auto_type
+#endif
 
+#if defined(__cplusplus)
+#define zdvar auto
+#else
+#define zdvar __auto_type
+#endif
+
+//MARK:- KeyPath
+#if DEBUG
+#define ZD_KEYPATH(object, property) ((void)(NO && ((void)object.property, NO)), @ #property)
+#else
+#define ZD_KEYPATH(object, property) @ #property
+#endif
+
+
+//MARK:- 警告消除
 /// 消除performSelector警告
 #define ZD_SuppressPerformSelectorLeakWarning(...)                      \
 do {                                                                    \
