b0b53b42b40154a81d7a04fe4c16138cb3c30d3f
faimin <fuxianchao@gmail.com>
2018-07-19 12:18:27 +0800

    update NSArray category

--- ZDToolKit/ZDCategory/Foundation/NSArray+ZDUtility.m
+++ ZDToolKit/ZDCategory/Foundation/NSArray+ZDUtility.m
@@ -27,26 +27,24 @@ ZD_AVOID_ALL_LOAD_FLAG_FOR_CATEGORY(NSArray_ZDUtility)
     return [self reverseObjectEnumerator].allObjects;
 }
 
-- (__kindof NSArray *)zd_shuffle {
+- (NSMutableArray *)zd_shuffle {
+    NSMutableArray *mutArr = [self isKindOfClass:[NSMutableArray class]] ? self : [self mutableCopy];
     if (self.count > 0) {
-        NSMutableArray *mutArr = [self isKindOfClass:[NSMutableArray class]] ? self : [self mutableCopy];
         for (NSUInteger i = self.count; i > 1; i--) {
             [mutArr exchangeObjectAtIndex:(i - 1)
                       withObjectAtIndex:arc4random_uniform((u_int32_t)i)];
         }
-        return mutArr;
     }
-    return self;
+    return mutArr;
 }
 
-- (__kindof NSArray *)zd_moveObjcToFront:(id)objc {
-    if ([self containsObject:objc]) {
-        NSMutableArray *mutArr = [self isKindOfClass:[NSMutableArray class]] ? self : [self mutableCopy];
-        [mutArr removeObject:objc];
-        [mutArr insertObject:objc atIndex:0];
-        return mutArr;
+- (NSMutableArray *)zd_moveObjcToFront:(id)obj {
+    NSMutableArray *mutArr = [self isKindOfClass:[NSMutableArray class]] ? self : [self mutableCopy];
+    if ([mutArr containsObject:obj]) {
+        [mutArr removeObject:obj];
+        [mutArr insertObject:obj atIndex:0];
     }
-    return self;
+    return mutArr;
 }
 
 - (NSArray *)zd_deduplication {
@@ -58,7 +56,7 @@ ZD_AVOID_ALL_LOAD_FLAG_FOR_CATEGORY(NSArray_ZDUtility)
 #endif
 }
 
-- (NSArray *)zd_collectSameElementWithArray:(__kindof NSArray *)otherArray {
+- (NSArray *)zd_collectSameElementWithArray:(NSArray *)otherArray {
     if (!otherArray || otherArray.count == 0) return @[];
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF IN %@", otherArray];
     NSArray *sameElements = [self filteredArrayUsingPredicate:predicate];
@@ -79,6 +77,14 @@ ZD_AVOID_ALL_LOAD_FLAG_FOR_CATEGORY(NSArray_ZDUtility)
 
 - (CGFloat)zd_min {
     return [[self valueForKeyPath:@"@min.floatValue"] floatValue];
+}
+
+- (void)zd_forEach:(void(^)(id, NSUInteger))block {
+    if (!block) return;
+    
+    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        block(obj, idx);
+    }];
 }
 
 - (NSMutableArray *)zd_map:(id (^)(id, NSUInteger))block {
