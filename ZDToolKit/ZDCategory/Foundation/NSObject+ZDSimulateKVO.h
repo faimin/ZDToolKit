11d664ad1f71441fa61cfd9731c24564ce3cfa88
faimin <fuxianchao@gmail.com>
2018-09-30 17:21:50 +0800

    fix typo

--- ZDToolKit/ZDCategory/Foundation/NSObject+ZDSimulateKVO.h
+++ ZDToolKit/ZDCategory/Foundation/NSObject+ZDSimulateKVO.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-//@warning: don't use with system's KVO together.
+//@warning: don't use it with system's KVO together.
 @interface NSObject (ZDSimulateKVO)
 
 - (void)zd_addObserver:(id)observer forKey:(NSString *)key callbackBlock:(void(^)(id observer, NSString *key, id newValue))block;
