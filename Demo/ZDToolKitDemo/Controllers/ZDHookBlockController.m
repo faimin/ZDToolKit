7fe253d0930d0205828dce7b04333234670550b2
faimin <fuxianchao@gmail.com>
2019-01-13 00:04:18 +0800

    change demo

--- Demo/ZDToolKitDemo/Controllers/ZDHookBlockController.m
+++ Demo/ZDToolKitDemo/Controllers/ZDHookBlockController.m
@@ -79,14 +79,22 @@ struct Block_layout {
 - (void)testHookBlock {
     __auto_type block = ^NSString *(NSString *name, NSUInteger age, id<NSObject> value) {
         NSString *blockResult = [NSString stringWithFormat:@"%@ + %ld + %@", name, age, value];
-        NSLog(@"block执行了，结果： %@", blockResult);
+        NSLog(@"block执行了: %@", blockResult);
         return blockResult;
     };
     
-    [self zd_hookBlock:&block hookWay:ZDHookWay_Libffi];
-    
-    NSString *result1 = block(@"Zero.D.Saber", 28, @100);
-    NSLog(@"执行结果1 = %@", result1);
+    {
+        [self zd_hookBlock:&block hookWay:ZDHookWay_Libffi];
+        
+        NSString *result1 = block(@"Zero.D.Saber", 28, @100);
+        NSLog(@"执行结果1 = %@", result1);
+    }
+    {
+        [self zd_hookBlock:&block hookWay:ZDHookWay_Libffi];
+        
+        NSString *result2 = block(@"ABC", 100, @123);
+        NSLog(@"执行结果2 = %@", result2);
+    }
     
     /*
     NSString *(*originFunc)(id blockSelf, NSString *name, NSUInteger age, NSNumber *) = (__typeof__(originFunc))originIMP;
